4b4d29cc8a5fcc4d9f869737595c28d2aa0c278f
tmtuan <tranminhtuan684@gmail.com>
2017-07-08 06:58:07 +0700

    Create PAckageCell to display Package Information

---
+++ Gyza2/PackageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PackageCell.swift
+//  Gyza2
+//
+//  Created by Tran Minh Tuan on 7/8/17.
+//  Copyright © 2017 Tran Minh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class PackageCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
