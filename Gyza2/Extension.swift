66cabc91a32646245740b4089d78e61ac8a2047f
tmtuan <tranminhtuan684@gmail.com>
2017-07-08 07:33:52 +0700

    AddContraintsWithFormat

---
+++ Gyza2/Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Extension.swift
+//  Gyza2
+//
+//  Created by Tran Minh Tuan on 7/8/17.
+//  Copyright © 2017 Tran Minh Tuan. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // Tuan's code: Add layout constraints to a view in relation with other views 
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
--- Gyza2/PackageCell.swift
+++ Gyza2/PackageCell.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class PackageCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    let thumbnailImageView: UIImageView = {
+        let imageView =  UIImageView()
+        imageView.backgroundColor = UIColor.gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -17,7 +26,12 @@ class PackageCell: UICollectionViewCell {
     
     func setupViews() {
         backgroundColor = UIColor.blue
+        addSubview(thumbnailImageView)
+        thumbnailImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         
+        addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-16-|", views: thumbnailImageView)
+       
     }
     
     required init?(coder aDecoder: NSCoder) {
