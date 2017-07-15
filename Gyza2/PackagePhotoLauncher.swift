657775208f38f04442227e8face1d51ab950acc8
tmtuan <tranminhtuan684@gmail.com>
2017-07-15 16:44:05 +0700

    Add basic animation to photo display screen

--- Gyza2/PackagePhotoLauncher.swift
+++ Gyza2/PackagePhotoLauncher.swift
@@ -18,7 +18,17 @@ class PackagePhotoLauncher: NSObject {
             let view = UIView(frame: keyWindow.frame)
             view.backgroundColor = UIColor.red
             
+            view.frame = CGRect(x: keyWindow.frame.width - 10, y: keyWindow.frame.height - 10, width: 10, height: 10)
+            
             keyWindow.addSubview(view)
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut,
+                           animations: {
+                            view.frame = keyWindow.frame
+            
+            }, completion: {(completedAnimation) in
+                // Do something here later
+            })
         }
      
     }
