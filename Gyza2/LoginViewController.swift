4d66bc2ffd43929cb525c9535487320361002942
tmtuan <tranminhtuan684@gmail.com>
2017-07-17 23:51:15 +0700

    Add Inputs Container to Login Screen

--- Gyza2/LoginViewController.swift
+++ Gyza2/LoginViewController.swift
@@ -9,14 +9,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor =  UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints  = false
+        
+        // make round corners
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.red
         
+        view.addSubview(inputsContainerView)
+    
+        setupInputsContainerView()
     }
 
+    func setupInputsContainerView() {
+        // Add constraints for inputsContainerView
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle  {
         get {
             return .lightContent
