e0ecbf0dc2b4a9580b6ac2d9e21b2824183d2384
tmtuan <tranminhtuan684@gmail.com>
2017-09-05 21:30:34 +0700

    Create LoginCell

--- Gyza2/LoginCell.swift
+++ Gyza2/LoginCell.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017 Tran Minh Tuan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
